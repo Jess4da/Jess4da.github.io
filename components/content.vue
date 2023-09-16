951f31093fbc6eb9a51521b90b59c158b73278ac
Jessada Duangpanyasawang <jessadad99@gmail.com>
2023-09-16 16:11:08 +0700

    add experience

--- components/content.vue
+++ components/content.vue
@@ -90,7 +90,7 @@ const { data: info } = await useAsyncData("info", () => queryContent("/info").fi
       id="about"
       class="flex justify-center items-start snap-always snap-start min-h-[100vh] h-fit w-full my-1"
     >
-      <div class="flex justify-center items-start my-[15vh] lg:my-[5vh] xl:my-[10vh]">
+      <div class="flex justify-center my-[15vh] lg:my-[5vh] xl:my-[10vh] w-full">
         <div class="flex flex-col justify-center w-[70%]">
           <p class="text-white text-5xl lg:text-9xl mb-5 font-semibold">About</p>
           <div class="flex">
@@ -102,7 +102,9 @@ const { data: info } = await useAsyncData("info", () => queryContent("/info").fi
             </p>
           </div>
           <p class="text-white text-3xl lg:text-5xl mt-10 font-semibold text-center">Skills</p>
-          <div class="grid grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 gap-2 lg:gap-5 mt-5">
+          <div
+            class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-3 lg:gap-5 mt-5"
+          >
             <div
               v-for="item in info?.about.skills"
               class="block aspect-square bg-slate-500 xl:hover:bg-[#FFEF63] rounded-2xl xl:transition xl:hover:scale-110"
@@ -116,13 +118,47 @@ const { data: info } = await useAsyncData("info", () => queryContent("/info").fi
         </div>
       </div>
     </div>
-    <div ref="div_portfolio" id="portfolio" class="flex justify-center items-start snap-always snap-start min-h-[100vh] h-fit w-full my-1">
-      Portfolio
+    <div
+      ref="div_portfolio"
+      id="portfolio"
+      class="flex justify-center items-start snap-always snap-start min-h-[100vh] h-fit w-full my-1"
+    >
+      <div class="flex justify-center my-[15vh] lg:my-[5vh] xl:my-[10vh] w-full">
+        <div class="flex flex-col w-[70%]">
+          <p class="text-white text-4xl lg:text-6xl mb-5 font-semibold">Experience</p>
+          <div class="flex justify-center">
+            <ol class="relative border-l border-gray-700 xl:max-w-[60%]">
+              <li v-for="item in info?.portfolio.experience" class="mb-10 ml-4">
+                <div
+                  class="absolute w-3 h-3 rounded-full mt-1.5 -left-1.5 border-none bg-[#FFEF63]"
+                ></div>
+                <time class="mb-1 text-sm lg:text-base font-normal leading-none text-gray-500">{{
+                  item.year
+                }}</time>
+                <h3 class="text-2xl lg:text-4xl font-semibold text-white">
+                  {{ item.title }}
+                </h3>
+                <p class="text-base lg:text-lg font-normal text-gray-300">
+                  {{ item.subtitle }}
+                </p>
+                <ul class="ms-10 list-disc text-sm lg:text-base font-light text-gray-400">
+                  <li v-for="d in item.detail">
+                    {{ d }}
+                  </li>
+                </ul>
+                <p class="text-sm lg:text-base font-light text-gray-300">
+                  Tech stack: <a class="font-semibold">{{ item.stack }}</a>
+                </p>
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
     </div>
     <div
       ref="div_contact"
       id="contact"
-      class="flex justify-center items-start snap-always snap-start min-h-[100vh] h-fit w-full my-1 "
+      class="flex justify-center items-start snap-always snap-start min-h-[100vh] h-fit w-full my-1"
     >
       Contact
     </div>
